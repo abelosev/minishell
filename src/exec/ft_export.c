d270ced4df9773dc923297efd6c7f487806a1783
Ana Belosevich <anabelosevich@MacBook-Air-Ana.local>
2024-06-13 17:56:41 +0200

    .h

--- src/exec/ft_export.c
+++ src/exec/ft_export.c
@@ -1,17 +1,22 @@
 #include "../../inc/minishell.h"
 
-void	ft_write_export(t_env *env, int fd)
+void	ft_write_export(t_list_env *env, int fd)
 {
-	t_env	*p;
+    char **envp;
+    int i;
 
-	p = env;
-	while (p)
-	{
-		write(fd, "declare -x ", 11);
-		write(fd, p->line, ft_strlen(p->line));
-		write(fd, "\n", 1);
-		p = p->next;
-	}
+    envp = get_envp(env);
+    if (!envp)
+        return ;
+    i = 0;
+    while (envp[i] != NULL)
+    {
+        ft_putstr_fd("declare -x ", fd);
+		ft_putstr_fd(envp[i], fd);
+		ft_putstr_fd("\n", fd);
+        i++;
+    }
+	free_tab(envp);
 }
 
 int	ft_export_is_valid(char *str)
@@ -20,9 +25,9 @@ int	ft_export_is_valid(char *str)
 
 	if (!ft_isalpha(str[0]) && str[0] != '_')
 	{
-		write(2, "minishell: export: `", 20);
-		write(2, str, ft_strlen(str));
-		write(2, "\': not a valid identifier\n", 26);
+		ft_putstr_err("minishell: export: `");
+		ft_putstr_err(str);
+		ft_putstr_err("\': not a valid identifier\n");
 		return (0);
 	}
 	i = 1;
@@ -30,9 +35,9 @@ int	ft_export_is_valid(char *str)
 	{
 		if (!ft_isalnum(str[i]) && str[i] != '_')
 		{
-			write(2, "minishell: export: `", 20);
-			write(2, str, ft_strlen(str));
-			write(2, "\': not a valid identifier\n", 26);
+			ft_putstr_err( "minishell: export: `");
+			ft_putstr_err(str);
+			ft_putstr_err("\': not a valid identifier\n");
 			return (0);
 		}
 		i++;
@@ -40,42 +45,22 @@ int	ft_export_is_valid(char *str)
 	return (1);
 }
 
-int	ft_export_replace_or_add(t_env *env, char *str)
-{
-	char	*str_copy;
-	t_env	*the_env;
-	int		len;
+////
 
-	len = 0;
-	while (str[len] != '=' && str[len] != '\0')
-		len++;
-	the_env = ft_find_in_env(env, str, len);
-	str_copy = ft_strdup(str);
-	if (!str_copy)
-		return (ft_error(NULL, 0, 1));
-	if (the_env != NULL)
-	{
-		free(the_env->line);
-		the_env->line = str_copy;
-		return (0);
-	}
-	return (ft_add_to_msh_env(env, str_copy));
-}
-
-int	ft_export(char **args, t_env *env, int fd)
+int	ft_export(t_group *group, t_list_env *env, int fd)
 {
 	int		i;
 	int		error_flag;
 
-	if (args[1] == NULL)
+	if (group->cmd[1] == NULL)
 		return (ft_write_export(env, fd), 0);
 	i = 1;
 	error_flag = 0;
-	while (args[i] != NULL)
+	while (group->cmd[i] != NULL)
 	{
-		if (ft_export_is_valid(args[i]))
+		if (ft_export_is_valid(group->cmd[i]))
 		{
-			if (ft_export_replace_or_add(env, args[i]))
+			if (ft_export_replace_or_add(env, group->cmd[i]))
 				return (1);
 		}
 		else
